16fcdc35e8aa7c1e1265afcd804f4c488c868971
Darryl <77030053+ddague1190@users.noreply.github.com>
2023-01-09 12:30:09 -0500

    Update views.py

--- bikes/views.py
+++ bikes/views.py
@@ -29,7 +29,7 @@ class PaginationWithCatIndices (LimitOffsetPagination):
             'results': data,
         })
 
-def resolve_csvfilter(self, queryset, name, value):
+def resolve_csvfilter(queryset, name, value):
     lookup = {f"{name}__in": value.split(",")}
     queryset = queryset.filter(**lookup)
     return queryset
